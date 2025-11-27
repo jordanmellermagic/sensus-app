6ef76c658eb455b93ed8e3e56a81400255242da1
jordanmellermagic <jordanmellermagic@gmail.com>
2025-11-26 16:06:23 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
+import SpectatorDataPage from "./pages/SpectatorDataPage";
+import PeekScreenPage from "./pages/PeekScreenPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProtectedRoute from "./ProtectedRoute";
 
-export default function App() {
+function App() {
   return (
-    <div className="w-full min-h-screen bg-black text-white">
-      <Outlet />
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <DashboardPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/spectator"
+        element={
+          <ProtectedRoute>
+            <SpectatorDataPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/peek"
+        element={
+          <ProtectedRoute>
+            <PeekScreenPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/settings"
+        element={
+          <ProtectedRoute>
+            <SettingsPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 }
+
+export default App;
